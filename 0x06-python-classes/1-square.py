ea41024803b5c6313382555883c8e9375191f56b
LordZeuz <chriskwesi12@gmail.com>
2022-12-21 02:02:53 +0000

    square correction

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,10 +6,9 @@
 class Square:
     """represents a square"""
 
-
-def __init__(self, size):
-    """Initialize a new Square.
+    def __init__(self, size):
+        """Initialize a new Square.
         Args:
             size (int): The size of the new square.
-    """
-    self.__size = size
+        """
+        self.__size = size

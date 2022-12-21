82dbbcf8a815a3ec59f535e7cfb8aa9a1ede3792
LordZeuz <chriskwesi12@gmail.com>
2022-12-21 01:46:25 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""creates a class named square"""
+
+
+class Square:
+    """represents a square"""
+
+
+def __init__(self, size):
+    """initialize with arg size which will be made private"""
+    self.__size = size

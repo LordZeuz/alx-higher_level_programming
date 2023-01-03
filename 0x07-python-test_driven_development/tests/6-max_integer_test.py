028facf875b0d2754158a0d96165598c3dc9a56e
LordZeuz <chriskwesi12@gmail.com>
2023-01-03 02:27:03 +0000

    unit test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
+"""Unittests for max_integer([..])."""
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
@@ -57,5 +57,5 @@ class TestMaxInteger(unittest.TestCase):
         """Test an empty string."""
         self.assertEqual(max_integer(""), None)
 
-
 if __name__ == '__main__':
+    unittest.main()

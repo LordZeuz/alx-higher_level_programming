9223b2bedcfc2cb3c09ef7f7b41ac7c6f2a8fa1c
LordZeuz <chriskwesi12@gmail.com>
2023-01-04 02:25:34 +0000

    rec delete count

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -15,6 +15,7 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 

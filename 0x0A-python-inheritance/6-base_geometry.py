e4dbe6ca4ee04efaf009a3ff6d5f4b396e93a05a
LordZeuz <chriskwesi12@gmail.com>
2023-01-10 03:03:10 +0000

    base geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,6 +5,7 @@
 class BaseGeometry:
 
     """block for geometry"""
+
     def area(self):
         """not yet defined"""
-    raise Exception("area() is not implemented")
+        raise Exception("area() is not implemented")
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
 
     def area(self):
         """block for area"""
-    raise Exception(area() is not implemented)
+        raise Exception(area() is not implemented)
 
     def integer_validator(self, name, value):
         """Validate a parameter as an integer.

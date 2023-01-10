8f3b1c87acd4cd115b7c675f7273f716859861af
LordZeuz <chriskwesi12@gmail.com>
2023-01-10 02:56:46 +0000

    base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""improved base geometry class"""
+
+
+class BaseGeometry:
+
+    """block for geometry"""
+    def area(self):
+        """not yet defined"""
+    raise Exception("area() is not implemented")

e16b752a809aff462ae7500ee6b7a599f1e57d28
LordZeuz <chriskwesi12@gmail.com>
2023-04-04 01:09:20 +0000

    request id

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
+"""
+import sys
+import urllib.request
+
+
+url = sys.argv[1]
+
+req = urllib.request.Request(url)
+req.add_header('X-Request-Id', 'my-unique-id')
+response = urllib.request.urlopen(url)
+request_id = response.getheader('X-Request-ID')
+print(request_id)

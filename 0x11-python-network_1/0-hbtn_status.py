cf3f083a99d72c2339d0117abb2c27ef97a68e5c
LordZeuz <chriskwesi12@gmail.com>
2023-04-04 00:40:57 +0000

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,3 +6,8 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     feedback = response.read()
+
+print("Body response:")
+print("\t- type: {}".format(type(feedback)))
+print("\t- content: {}".format(feedback))
+print("\t- utf8 content: {}".format(feedback.decode('utf-8')))

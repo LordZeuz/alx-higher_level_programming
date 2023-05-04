135ef8b7ac703f080b50871b0318d8391d5f00b6
LordZeuz <chriskwesi12@gmail.com>
2023-05-04 17:09:11 +0000

    semi standard

--- 0x14-javascript-web_scraping/demo.js
+++ 0x14-javascript-web_scraping/demo.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-//const eps = process.argv[2];
-const url = 'https://www.youtube.com';
-request(url, (error,response, body) => {
-    console.log(`code: ${response.statusCode}`);
- //   const almost = JSON.parse(body);
-    console.log(body);
+const eps = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request(url + eps, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  const obj = JSON.parse(body);
+  console.log(obj.title);
 });
